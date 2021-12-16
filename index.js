c15af3f312a9292dd5eff7845a23ba4ddd4aa0d8
Johan-Ortega118 <85240547+Johan-Ortega118@users.noreply.github.com>
2021-12-16 00:30:57 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 require('dotenv').config();
 
 const server = require('./server');
-const PORT = process.env.PORT || 8126;
+const PORT = process.env.PORT || 8123;
 
-server.listen(PORT, ()=> console.log('Server is live at localhost: 8126'));
+server.listen(PORT, ()=> console.log('Server is live at localhost: 8123'));
